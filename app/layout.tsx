b15897f6663f22550f6a73574f8706b664e74b3e
meuzi8122 <kagiakete1234@gmail.com>
2022-11-26 20:02:14 +0900

    return忘れ

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,19 @@ import Header from "../components/navbar/header";
 import "../styles/globals.css";
 
 export default ({ children }: LayoutProps) => {
-    
-    <html lang="ja">
-        <head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>PM Songs</title>
-        </head>
-        <body>
-            <Header />
-            {children}
-        </body>
-    </html>
+
+    return (
+        <html lang="ja">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>PM Songs</title>
+            </head>
+            <body>
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 
 }
