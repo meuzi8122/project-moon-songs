6ea2fd81c8d73ff0e0022230fe2dd8a3001fe967
meuzi8122 <kagiakete1234@gmail.com>
2022-11-26 19:46:26 +0900

    カードのボタンにアイコン追加

--- components/card/song.tsx
+++ components/card/song.tsx
@@ -1,18 +1,23 @@
+import Image from "next/image";
+import Youtube from "../../assets/images/youtube.svg";
+
 export default ({ song }: SongCardProps) => {
 
     return (
-        <div className="card card-side bg-base-300 rounded-box">
+        <div className="card card-side bg-base-300 rounded-box min-width-5xl">
             <figure>
                 <img src={song.artworkUrl} />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{song.title}</h2>
                 <div className="flex space-x-2">
-                    <span className="badge badge-accent">{song.game}</span>
-                    <span className="badge badge-accent">{song.kind}</span>
+                    <p>{`${song.game} / ${song.kind}`}</p>
                 </div>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
+                    <a href={song.youtubeUrl} target="_blank" className="btn btn-primary gap-2">
+                        <Image alt="YoutubeVideo" src={Youtube} />
+                        Play
+                    </a>
                 </div>
             </div>
         </div>
