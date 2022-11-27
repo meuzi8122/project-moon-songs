65188c90e782e088b44afdf8e27d04862626a63b
meuzi8122 <kagiakete1234@gmail.com>
2022-11-27 20:13:01 +0900

    型

--- @types/prop.d.ts
+++ @types/prop.d.ts
@@ -2,6 +2,12 @@ type LayoutProps = {
     children: React.ReactElement;
 }
 
+type SongPageProps = {
+    params: { 
+        gameId: string;
+    }
+}
+
 type SongCardProps = {
     song: Song;
 }
--- app/songs/[gameId]/page.tsx
+++ app/songs/[gameId]/page.tsx
@@ -1,21 +1,16 @@
-import { use } from "react";
 import { SongService } from "../../../cms/song";
 import SongBoard from "../../../components/board/song";
-import SongCard from "../../../components/card/song";
 import { GAMES } from "../../../constants/domain";
 
-export default async ({ params }: { params: { gameId: string } }) => {
+export default async ({ params }: SongPageProps) => {
 
     const { gameId } = params;
     const game = GAMES[gameId];
-    //const songs = use(SongService.findSongs(GAMES[gameId]));
     const songs = await SongService.findSongs(game);
 
     return (
         <div className="mt-12">
-
             <SongBoard game={game} songs={songs} />
-
         </div>
     )
 
